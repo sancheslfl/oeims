40ba43201bedaeb42238086a4dc858ffe955041d
Miguel Sanches <a49337@alunos.isel.pt>
2026-03-27 02:13:27 +0000

    feat: NetworkManager 1st version

---
+++ Daemon/Monitors/NetworkMonitor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Daemon.Monitors
+{
+    internal class NetworkMonitor
+    {
+        private string? _initialNetworkId;
+        private HashSet<string> _initialInterfaces = new();
+
+        public void InitializeBaseline()
+        {
+            _initialNetworkId = GetCurrentNetworkId();
+            _initialInterfaces = GetActiveInterfaces();
+        }
+
+        public bool HasNetworkChanged()
+        {
+            var current = GetCurrentNetworkId();
+            return current != _initialNetworkId;
+        }
+
+        public bool HasMultipleInterfaces()
+        {
+            var current = GetActiveInterfaces();
+            return !_initialInterfaces.SetEquals(current);
+        }
+
+        public bool HasMultipleActiveNetworks()
+        {
+            var count = GetActiveInterfaces().Count;
+            return count > 1;
+        }
+
+        private string GetCurrentNetworkId()
+        {
+            var active = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up &&
+                            n.NetworkInterfaceType != NetworkInterfaceType.Loopback);
+
+            return string.Join("|", active.Select(n =>
+            {
+                var ipProps = n.GetIPProperties();
+                var gateway = ipProps.GatewayAddresses
+                    .FirstOrDefault()?.Address.ToString() ?? "no-gw";
+
+                return $"{n.Name}-{gateway}";
+            }));
+        }
+
+        private HashSet<string> GetActiveInterfaces()
+        {
+            return NetworkInterface.GetAllNetworkInterfaces()
+                .Where(n => n.OperationalStatus == OperationalStatus.Up &&
+                            n.NetworkInterfaceType != NetworkInterfaceType.Loopback)
+                .Select(n => n.Name)
+                .ToHashSet();
+        }
+    }
+}
