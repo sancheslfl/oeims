31b992701f045e3e7319e82e5b6835bd8345f0b4
Dinis Contreiras <diniscontreiras@gmail.com>
2026-05-30 15:34:17 +0100

    feat: add SessionSupervisors juntcion table

--- Server/src/main/kotlin/com/oeims/plugins/Database.kt
+++ Server/src/main/kotlin/com/oeims/plugins/Database.kt
@@ -21,6 +21,7 @@ fun Application.configureDatabase() {
             Users,
             Exams,
             Sessions,
+            SessionSupervisors,
             Participants,
             Events
         )
