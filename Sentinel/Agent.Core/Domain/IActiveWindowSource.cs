e498d26747646f342d44ff41cd428a024ed2304c
Miguel Sanches <101569427+sancheslfl@users.noreply.github.com>
2026-07-11 19:42:37 +0100

    refactor: merge Sentinel Agent projects

--- Sentinel/Agent.Core/Domain/IActiveWindowSource.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace OEIMS.Sentinel.Agent.Domain
-{
-    public interface IActiveWindowSource : IDisposable
-    {
-        Task StartAsync(Func<ActiveWindowInfo, Task> onChanged, CancellationToken ct);
-    }
-}
--- Sentinel/Agent/Mitigators/ClipboardBlocker.cs
+++ Sentinel/Agent/Mitigators/ClipboardBlocker.cs
@@ -3,6 +3,12 @@ using OEIMS.Sentinel.Agent.Domain;
 
 namespace OEIMS.Sentinel.Agent.Mitigators;
 
+/// <summary>
+/// Prevents clipboard acces.
+/// </summary>
+/// <param name="clipboardSource">
+/// Platform implementation that offers clipboard interaction.
+/// </param>
 internal sealed class ClipboardBlocker(IClipboardSource clipboardSource) : IMitigator
 {
     public string Name => nameof(ClipboardBlocker);
