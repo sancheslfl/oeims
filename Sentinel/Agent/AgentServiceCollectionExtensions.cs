5b3885a6857757845451d30c781f514b1b2267a9
Miguel Sanches <a49337@alunos.isel.pt>
2026-07-13 12:31:51 +0100

    fix: service collection extensions

---
+++ Sentinel/Agent/AgentServiceCollectionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using OEIMS.Sentinel.Agent.Domain;
+using OEIMS.Sentinel.Agent.Ipc;
+using OEIMS.Sentinel.Agent.Mitigators;
+using OEIMS.Sentinel.Agent.Monitors;
+using OEIMS.Sentinel.Agent.Platform.Windows;
+
+namespace OEIMS.Sentinel.Agent;
+
+public static class AgentServiceCollectionExtensions
+{
+    public static IServiceCollection AddAgent(this IServiceCollection services)
+    {
+        services.AddSingleton<IActiveWindowSource, WinActiveWindowSource>();
+        services.AddSingleton<IClipboardSource, WinClipboardSource>();
+        services.AddSingleton<IExamIdentityCodeOverlay, ExamIdentityCodeOverlay>();
+
+        services.AddSingleton<AgentEventPipeClient>();
+        services.AddSingleton<AgentCommandPipeServer>();
+
+        services.AddSingleton<IMonitor, FocusMonitor>();
+        services.AddSingleton<IMitigator, ClipboardBlocker>();
+
+        services.AddHostedService<Worker>();
+
+        return services;
+    }
+}
